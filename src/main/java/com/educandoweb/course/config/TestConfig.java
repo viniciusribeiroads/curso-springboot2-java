63ef99f8425572300b7c9c092c1aa71796300f53
Vinicius Salvador Ribeiro <viniciusribeiro113@gmail.com>
2022-01-27 22:06:09 -0300

    JPA repository, depedency injection, dabase seeding

---
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -0,0 +1,36 @@
+package com.educandoweb.course.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
+
+@Configuration
+@Profile("test") //Classe de configuracao de teste, vamos usar ela 
+public class TestConfig implements CommandLineRunner {
+	
+	//precisaremos do Repository pois vamos mexer com o BD, entao vamos fazer uma injecao de dependencia
+	
+	@Autowired //anotacao de dependencia, ele vai associar uma instancia do userRepository
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456"); 
+		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		
+		userRepository.saveAllAndFlush(Arrays.asList(u1, u2));
+	}
+	
+	
+	
+	
+	
+
+}
